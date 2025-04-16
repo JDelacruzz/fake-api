b3322322fa4e25fdd0fc5afd87f0b4b98d5be6bf
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-04-15 19:55:57 -0500

    ✨ feat: Implementar la visualización del carrito de compras y la gestión de productos

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartStateService } from '../../services/cart-state.service';
+import { CartProductComponent } from "../../components/cart-product/cart-product.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartProductComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
-export default class CartComponent {}
+export default class CartComponent {
+  state = inject(CartStateService).state;
+}
